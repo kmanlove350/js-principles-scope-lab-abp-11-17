432631dd93e83518ea6dfa3e0e4b0ec7fa6b8521
Kaitlyn Manlove <kaitlyn.manlove@me.com>
2017-11-16 02:56:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution in this file!
 var customerName = 'Kaitlyn'
 function upperCaseCustomerName()  {
-  return customerName.toUpperCase()
+  customerName.toUppercase()
 }
 
 function setBestCustomer()  {
